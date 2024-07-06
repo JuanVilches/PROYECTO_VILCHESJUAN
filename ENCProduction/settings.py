0a62a606bea1ff626e3b8cfdb7c01cc885f25443
Juan Vilches <juan.vilches@usm.cl>
2024-07-06 01:29:19 -0400

    agegamos las aplicaciones necesarias y rest_framework al settings.py

--- ENCProduction/settings.py
+++ ENCProduction/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'planta',
+    'producto',
+    'produccion',
+    'rest_framework'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
